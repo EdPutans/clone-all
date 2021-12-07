5a327733dfb66ffd6ccbe8266dbba3f3b5b7b158
Ed 'Quacker' Putans <edputans@gmail.com>
2021-12-07 15:26:25 +0000

    fixed

--- clone-all.js
+++ clone-all.js
@@ -1,9 +1,11 @@
 import { execSync } from 'child_process';
 import { existsSync } from 'fs';
-import { exit } from 'process';
+import { exit, stdin, stdout } from 'process';
 import readline from 'readline';
 
-const io = readline.createInterface({ input: process.stdin, output: process.stdout });
+const io = readline.createInterface({ input: stdin, output: stdout });
+
+const truthyUserInput = ['y', 'yes', 'Y', 'Yes'];
 
 const userNames = [
   // hehe nope.
@@ -11,21 +13,19 @@ const userNames = [
   // just make a list of strings 
 ];
 
-const cleanFolder = (repoName, userName) => execSync(`cd ${repoName} && \
-rm -rf ${userName} && \
-mkdir ${userName}`);
+const cleanFolder = (repoName, userName) => execSync(`cd ${repoName} && rm -rf ${userName} && mkdir ${userName}`);
 
-const erroredUsernames = [];
 
-const cloneForAll = (repoName, shouldInstall, shouldOverrideExistingFolders) => {
+// returms an object with the errored usernames.
+const cloneForAll = async (repoName, shouldInstall, shouldOverrideExistingFolders) => {
   // if folder doesn't exist, create it
-  if (!existsSync(repoName)) {
-    execSync(`mkdir ${repoName}`);
-    console.log('Folder created');
-  }
+  if (!existsSync(repoName)) execSync(`mkdir ${repoName}`);
+  
+
+  const erroredUsernames = [];
 
   // loop over every person
-  userNames.forEach((userName) => {
+  await userNames.forEach((userName) => {
     const localPath = `${repoName}/${userName}`;
 
     try {
@@ -34,50 +34,49 @@ const cloneForAll = (repoName, shouldInstall, shouldOverrideExistingFolders) => 
         if (shouldOverrideExistingFolders) {
           console.log(`Cleaning ${localPath}...`);
           cleanFolder(repoName, userName);
-          // skip otherwise
         } else {
+          // skip otherwise
           console.log(`${userName} already cloned. Skipping...`);
           return;
         }
       }
-
       // clone the repo
-      return execSync(`
-            cd ${repoName} && \
-            git clone git@github.com:${userName}/${repoName}.git ${userName} \
-            ${shouldInstall ? `&& cd ${userName} && npm install` : ''}
-        `);
-    } catch {
-      execSync(`cd ${repoName} && rm -rf ${repoName}`);
+      return execSync(`cd ${repoName} && git clone git@github.com:${userName}/${repoName}.git ${userName} ${shouldInstall ? `&& cd ./${userName} && npm install` : ''}`)
+
+    } catch (e) {
       // add to errored usernames if wasn't able to clone
       erroredUsernames.push(userName);
     }
   });
 
-  return true;
+  return { erroredUsernames };
 }
 
 // main
 const init = async () => {
+  if (!userNames.length) {
+    console.error('No usernames provided!');
+    exit(1);
+  }
   // ask for repo name
   await io.question('Repo name: ', async (repoName) => {
     // ask for whether to override the existing clones
     await io.question('Should override existing folders? (y/n): ', async (overrideExisting) => {
-      const shouldOverrideExistingFolders = overrideExisting === 'y' || overrideExisting === 'yes' || overrideExisting === 'Y' || overrideExisting === 'Yes';
+      const shouldOverrideExistingFolders = truthyUserInput.includes(overrideExisting);
 
       // ask for whether to run NPM INSTALL on each project
-      await io.question('Run npm install? [y/n] ', async runInstall => {
-        const shouldInstall = runInstall === 'y' || runInstall === 'yes' || runInstall === 'Y' || runInstall === 'Yes';
+      await io.question('Run npm install in each project? [y/n] ', async runInstall => {
+        const shouldInstall = truthyUserInput.includes(runInstall);
 
-        // clone everybody's repos
-        cloneForAll(repoName, shouldInstall, shouldOverrideExistingFolders);
+        // clone everybody's repos and get the errored names
+        const { erroredUsernames } = await cloneForAll(repoName, shouldInstall, shouldOverrideExistingFolders);
 
         // if there were any errors, print them
         if (erroredUsernames.length) {
           console.error(`Couldn't fetch the repo for ${erroredUsernames.join(', ')}. Removing their empty folders...`);
 
+          // remove the empty folders and exit with an error
           erroredUsernames.forEach(userName => {
-            // remove the empty folders
             execSync(`cd ${repoName} && rm -rf ${userName}`);
           })
           exit(1);
@@ -85,12 +84,12 @@ const init = async () => {
         } else {
           // surprise - surprise, everybody submitted their work!
           console.info(`No way, everybody submitted today!`);
-          exit(0)
+          exit(0);
         }
       });
     })
   });
 }
 
-// run me
+// so run me baby
 init();
